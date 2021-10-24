f6713d0de95cf71f4d564f0b5fb566650cd47da9
Mizack Mianuti <mizack1234@gmail.com>
2021-10-24 20:56:56 -0300

    youtube parcial

--- youtube.py
+++ youtube.py
@@ -1,89 +1,112 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from pai import Acessar_web
+from datetime import datetime
 import time
+import pandas as pd
 url = 'https://www.youtube.com/channel/UCYfdidRxbB8Qhf0Nx7ioOYw'
-
+# url = 'https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ'
+# identi = '/html/body/ytd-app/div/ytd-page-manager/ytd-browse[4]/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[5]/div[3]/ytd-shelf-renderer/div[1]'
+identi = '//*[@id="dismissible"]'
 class Youtube(Acessar_web):
     def __init__(self,url):
-        # acessando superclasse
+        # acessando método da superclasse
         super().__init__(url)
         # instanciando navgador
         nav = self.navegador
         self.nav = nav
         
 
-    def acessar_codigo_html(self):
+    def acessar_codigo_html(self,identificador):
         # idicando local em que os vídeos se encontram
-        videos = self.nav.find_element_by_xpath('//*[@id="dismissible"]')
-        # clicando na seta para pegar todos os vídeos selecionados
-        self.nav.find_element_by_xpath('//*[@id="right-arrow"]/ytd-button-renderer').click()
-        # salvando dados da pagina
-        html = videos.get_attribute('outerHTML')
-        # fechando wd
-        self.nav.quit()
-        return html
+        time.sleep(10)
+        try:
+            
+            videos = self.nav.find_element_by_xpath(identificador)
+            # clicando na seta para pegar todos os vídeos selecionados
+            self.nav.find_element_by_xpath('//*[@id="right-arrow"]/ytd-button-renderer').click()
+            # salvando dados da pagina
+            html = videos.get_attribute('outerHTML')
+            print(html)
+            # fechando wd
+            self.nav.quit()
+            return html
+        except:
+            self.nav.quit()
+            print('Falha na captura de dados')
+
 
     def separar_dados(self):
         # parseando dados
-        soup = BeautifulSoup(self.acessar_codigo_html(),'html.parser')
+        soup = BeautifulSoup(self.acessar_codigo_html(identi),'html.parser')
         titulos_pg = soup.find(name='span',attrs={'id':'title'})
         titulos = soup.find_all(name='a',attrs={'id':'video-title'})
+        print(titulos)
         canais = soup.find_all(name='a',attrs={'class':'yt-simple-endpoint style-scope yt-formatted-string'})
         data_postages_views = soup.find_all(name='span',attrs={'class':'style-scope ytd-grid-video-renderer'})
-        tempo_videos = soup.find_all(name='span',attrs={'id':'text'})
         links = soup.find_all(name='a',attrs={'id':'thumbnail'})
+        return titulos_pg,titulos,canais,data_postages_views,links
+        # tempo_videos = soup.find_all(name='span',attrs={'id':'text'})
+
+
+    def extrair_dados_coletados(self,dados,link=None,separador=None):
+        lista_dados = []
+        visualizacoes =[]
+        contagem = 0
+        for dado in dados:
+
+            # separador padrão
+            if link == None and separador == None:
+                valor = dado.text.strip()
+                lista_dados.append(valor)
+                
+            # separador de link
+            elif link != None and separador == None:
+                valor = dado.get('href')
+                lista_dados.append(valor)
+
+            # separador visualização e tempo postagem
+            elif link == None and separador != None:
+                valor = dado.text.strip()
+                # tempo postagem
+                if contagem%2 != 0:
+                    lista_dados.append(valor)
+                # visualizações
+                else:
+                    visualizacoes.append(valor)
+            contagem += 1
+
+        # separador de retorno
+        if link == None and separador != None:
+            return lista_dados,visualizacoes
+        else:
+            return lista_dados
+
 
     def data_agora(self):
-        pass
+        return datetime.today()
 
-
-
-
-# aaa = Youtube(url).separar_dados()
-
-
-# instanciando navegador
-# navegador = webdriver.Firefox()
-# navegador.get(url)
-# # selecionando dados
-# videos = navegador.find_element_by_xpath('//*[@id="dismissible"]')
-# navegador.find_element_by_xpath('//*[@id="right-arrow"]/ytd-button-renderer').click()
-# html = videos.get_attribute('outerHTML')
-# # fechando navegador
-# navegador.quit()
-
-# soup = BeautifulSoup(html,'html.parser')
-# tempo_videos = soup.find_all(attrs={'class':'style-scope ytd-thumbnail-overlay-time-status-renderer','class':'style-scope ytd-thumbnail-overlay-time-status-renderer','class':'style-scope ytd-thumbnail-overlay-time-status-renderer'})#,attrs={'class':'style-scope ytd-thumbnail-overlay-time-status-renderer'})
-
-# titulo_pg = soup.find(name='span',attrs={'id':'title'})
-# titulos = soup.find_all(name='a',attrs={'id':'video-title'})
-# kenells = soup.find_all(name='a',attrs={'class':'yt-simple-endpoint style-scope yt-formatted-string'})
-# data_postages_views = soup.find_all(name='span',attrs={'class':'style-scope ytd-grid-video-renderer'})
-# links = soup.find_all(name='a',attrs={'id':'thumbnail'})
-
-# contagem = 0
-# for sea in data_postages_views:
     
-#     teste = sea.text.strip()
-#     if contagem%2 != 0:
-#         print(contagem,'\t',teste,'\n\n\n')
-#     # else:
-#     #     print(contagem,'\t',teste,'\n\n\n')
-#     contagem += 1
-# print(contagem)
+    def gerar_csv(self):
+        titulos_pg,titulos,canais,data_postages_views,links = self.separar_dados()
+        title = self.extrair_dados_coletados(titulos_pg)
+        name = self.extrair_dados_coletados(titulos)
+        channel = self.extrair_dados_coletados(canais)
+        time,view = self.extrair_dados_coletados(data_postages_views,None,'dados')
+        link = self.extrair_dados_coletados(links,'dados')
 
-# contagem2 = 0
-# for kenell2 in links:
-#     kenell3 = kenell2.get('href')
-#     print(contagem2,'\t',kenell3,'\n\n\n')
-#     contagem2 += 1
-# print(contagem2)
-# print(titulo_pg)
+        df = pd.DataFrame({'TITULO':name,'CANAL':channel,'TEMPO_POSTAGEM':time,'VISUALIZACOES':view,'LINK_VIDEO':link,'DATA_ACESSO':self.data_agora()})
+        print(df)
+        title_alt = str(title).replace('[','').replace(']','').replace("'","").replace('í','i').replace(' ','_')
+        df.to_csv(str(title_alt+'.csv'),index=False,sep='\t')
+        
+        
+    def ler_arq_final(self):
+        dados = pd.read_csv('Novos_videos.csv',sep='\t')
+        return dados
 
-# contagem2 = 0
-# for kenell2 in tempo_videos:
-#     kenell3 = kenell2.text.strip()
-#     print(contagem2,'\t',kenell3,'\n\n\n')
-#     contagem2 += 1
-# print(tempo_videos)
+
+ytb = Youtube(url)
+ytb.gerar_csv()
+
+
